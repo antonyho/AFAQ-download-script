263df046c42f1792adedbf1e6317e690e977e77d
antonyho <ntonyworkshop@gmail.com>
2014-03-11 14:19:37 +0800

    Appendix 3.4 from the source website is corrupted
    Skip merging this section until maintainer fixs it.

--- download_merge_anarchist_faq.sh
+++ download_merge_anarchist_faq.sh
@@ -24,7 +24,7 @@ wget -c -P ~/AnarchistFAQ/sections http://www.infoshop.org/faq/append3.pdf
 wget -c -P ~/AnarchistFAQ/sections http://www.infoshop.org/faq/append31.pdf
 wget -c -P ~/AnarchistFAQ/sections http://www.infoshop.org/faq/append32.pdf
 wget -c -P ~/AnarchistFAQ/sections http://www.infoshop.org/faq/append33.pdf
-wget -c -P ~/AnarchistFAQ/sections http://www.infoshop.org/faq/append34.pdf
+#wget -c -P ~/AnarchistFAQ/sections http://www.infoshop.org/faq/append34.pdf
 wget -c -P ~/AnarchistFAQ/sections http://www.infoshop.org/faq/append35.pdf
 wget -c -P ~/AnarchistFAQ/sections http://www.infoshop.org/faq/append4.pdf
 wget -c -P ~/AnarchistFAQ/sections http://www.infoshop.org/faq/append41.pdf
@@ -35,7 +35,7 @@ wget -c -P ~/AnarchistFAQ/sections http://www.infoshop.org/faq/append45.pdf
 wget -c -P ~/AnarchistFAQ/sections http://www.infoshop.org/faq/append46.pdf
 wget -c -P ~/AnarchistFAQ/sections http://www.infoshop.org/faq/biblio.pdf
 
-pdfunite ~/AnarchistFAQ/sections/intro.pdf ~/AnarchistFAQ/sections/10years.pdf ~/AnarchistFAQ/sections/vol1intro.pdf ~/AnarchistFAQ/sections/sectionA.pdf ~/AnarchistFAQ/sections/sectionB.pdf ~/AnarchistFAQ/sections/sectionC.pdf ~/AnarchistFAQ/sections/sectionD.pdf ~/AnarchistFAQ/sections/sectionE.pdf ~/AnarchistFAQ/sections/sectionF.pdf ~/AnarchistFAQ/sections/sectionG.pdf ~/AnarchistFAQ/sections/sectionH.pdf ~/AnarchistFAQ/sections/sectionI.pdf ~/AnarchistFAQ/sections/sectionJ.pdf ~/AnarchistFAQ/sections/append1.pdf ~/AnarchistFAQ/sections/append11.pdf ~/AnarchistFAQ/sections/append12.pdf ~/AnarchistFAQ/sections/append13.pdf ~/AnarchistFAQ/sections/append2.pdf ~/AnarchistFAQ/sections/append3.pdf ~/AnarchistFAQ/sections/append31.pdf ~/AnarchistFAQ/sections/append32.pdf ~/AnarchistFAQ/sections/append33.pdf ~/AnarchistFAQ/sections/append34.pdf ~/AnarchistFAQ/sections/append35.pdf ~/AnarchistFAQ/sections/append4.pdf ~/AnarchistFAQ/sections/append41.pdf ~/AnarchistFAQ/sections/append42.pdf ~/AnarchistFAQ/sections/append43.pdf ~/AnarchistFAQ/sections/append44.pdf ~/AnarchistFAQ/sections/append45.pdf ~/AnarchistFAQ/sections/append46.pdf ~/AnarchistFAQ/sections/biblio.pdf ~/AnarchistFAQ/AnarchistFAQ-full.pdf
+pdfunite ~/AnarchistFAQ/sections/intro.pdf ~/AnarchistFAQ/sections/10years.pdf ~/AnarchistFAQ/sections/vol1intro.pdf ~/AnarchistFAQ/sections/sectionA.pdf ~/AnarchistFAQ/sections/sectionB.pdf ~/AnarchistFAQ/sections/sectionC.pdf ~/AnarchistFAQ/sections/sectionD.pdf ~/AnarchistFAQ/sections/sectionE.pdf ~/AnarchistFAQ/sections/sectionF.pdf ~/AnarchistFAQ/sections/sectionG.pdf ~/AnarchistFAQ/sections/sectionH.pdf ~/AnarchistFAQ/sections/sectionI.pdf ~/AnarchistFAQ/sections/sectionJ.pdf ~/AnarchistFAQ/sections/append1.pdf ~/AnarchistFAQ/sections/append11.pdf ~/AnarchistFAQ/sections/append12.pdf ~/AnarchistFAQ/sections/append13.pdf ~/AnarchistFAQ/sections/append2.pdf ~/AnarchistFAQ/sections/append3.pdf ~/AnarchistFAQ/sections/append31.pdf ~/AnarchistFAQ/sections/append32.pdf ~/AnarchistFAQ/sections/append33.pdf ~/AnarchistFAQ/sections/append35.pdf ~/AnarchistFAQ/sections/append4.pdf ~/AnarchistFAQ/sections/append41.pdf ~/AnarchistFAQ/sections/append42.pdf ~/AnarchistFAQ/sections/append43.pdf ~/AnarchistFAQ/sections/append44.pdf ~/AnarchistFAQ/sections/append45.pdf ~/AnarchistFAQ/sections/append46.pdf ~/AnarchistFAQ/sections/biblio.pdf ~/AnarchistFAQ/AnarchistFAQ-full.pdf
 
 
 echo -e "\e[0;31mDone!\e[0m"
